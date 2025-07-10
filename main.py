9a71fe950be38fead9b1b81abc27939558e206ed
Chris <chris35298@gmail.com>
2025-07-10 00:09:10 -0300

    nuevo

--- main.py
+++ main.py
@@ -3,18 +3,18 @@ from domain.dataset_csv import DatasetCSV
 from domain.dataset_excel import DatasetEXCEL
 from data.data_saver import DataSaver
 
-csv_path = path.join(path.dirname(__file__), "files/snic-provincias.csv")
+csv_path = path.join(path.dirname(__file__), "files/SAT2021-2023.csv")
 excel_path = path.join(path.dirname(__file__), "files/hechos.xlsx")
 
 csv = DatasetCSV(csv_path)
 csv.cargar_datos()
 
 
-excel = DatasetEXCEL(excel_path)
-excel.cargar_datos()
+# excel = DatasetEXCEL(excel_path)
+# excel.cargar_datos()
 
 #gurdado en base de datos
 db = DataSaver()
-db.guardar_dataframe(csv.datos, "snic")  
-db.guardar_dataframe(excel.datos, "hechos") 
+db.guardar_dataframe(csv.datos, "SAT2021_2023")  
+# db.guardar_dataframe(excel.datos, "hechos") 
 
